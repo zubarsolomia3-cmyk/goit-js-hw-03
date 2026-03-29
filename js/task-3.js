9c88dc30b20d5202c9f43b2fa1bbd3ee3cc33e1a
zubarsolomia3-cmyk <zubarsolomia3@gmail.com>
2026-03-29 17:17:25 +0300

    task3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,17 @@
+function filterArray(numbers, value) {
+  const result = [];
 
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      result.push(numbers[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
